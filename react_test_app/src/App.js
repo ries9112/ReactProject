b947809d2b526817ee581f37f2b0fb72957ee606
Riccardo Esclapon <ries9112@colorado.edu>
2020-06-11 12:37:11 -0600

    progress through page 16 of book

---
+++ react_test_app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function getTitle(title){
+  return title;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello {getTitle('NewTitle')}
+      </h1>
+      
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+export default App;
